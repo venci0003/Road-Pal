1d45006c8f88d10f69ed6189f28ec3cec4fb9894
venci0003 <venci0003@gmail.com>
2024-08-24 14:14:45 +0300

    Added common project for app constants.

---
+++ RoadPal.Common/ApplicationConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace RoadPal.Common
+{
+	public class ApplicationConstants
+	{
+		public static class DatabaseConstants
+		{
+			public const string DatabaseFilename = "TodoSQLite.db3";
+
+			public const SQLite.SQLiteOpenFlags Flags =
+				// open the database in read/write mode
+				SQLite.SQLiteOpenFlags.ReadWrite |
+				// create the database if it doesn't exist
+				SQLite.SQLiteOpenFlags.Create |
+				// enable multi-threaded database access
+				SQLite.SQLiteOpenFlags.SharedCache;
+
+			public static string DatabasePath =>
+				Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+		}
+	}
+}
---
+++ RoadPal.Common/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace RoadPal.Common
+{
+	// All the code in this file is only included on Mac Catalyst.
+	public class PlatformClass1
+	{
+	}
+}
