6c0e869a30907851010ee1c6d456bfa6bd57b268
venci0003 <venci0003@gmail.com>
2024-08-24 14:16:08 +0300

    Added infrastructure project for database.

---
+++ RoadPal.Infrastructure/Platforms/Android/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace RoadPal.Infrastructure
+{
+	// All the code in this file is only included on Android.
+	public class PlatformClass1
+	{
+	}
+}
---
+++ RoadPal.Infrastructure/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace RoadPal.Infrastructure
+{
+	// All the code in this file is only included on Mac Catalyst.
+	public class PlatformClass1
+	{
+	}
+}
