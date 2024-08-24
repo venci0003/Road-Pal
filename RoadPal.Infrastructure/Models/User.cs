a52c0af2122ef4671640754a1d43f005719e9129
venci0003 <venci0003@gmail.com>
2024-08-24 14:16:26 +0300

    Added user model.

---
+++ RoadPal.Infrastructure/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace RoadPal.Infrastructure.Models
+{
+	public class User
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+
+		public string Username { get; set; }
+
+		public string Email { get; set; }
+	}
+}
