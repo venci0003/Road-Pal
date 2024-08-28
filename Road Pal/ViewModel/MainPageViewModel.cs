a783deb3032929116b65f5d469d5e2bbdbb2f56a
venci0003 <venci0003@gmail.com>
2024-08-28 16:38:35 +0300

    Updated MainPage and created a MainPage viewmodel.

---
+++ Road Pal/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RoadPal.Infrastructure.Models;
+using RoadPal.Services;
+using System.Collections.ObjectModel;
+
+namespace RoadPal.ViewModels
+{
+	public partial class MainPageViewModel : ObservableObject
+	{
+		private readonly CarService _carService;
+
+		[ObservableProperty]
+		private ObservableCollection<Car>? cars;
+
+		[ObservableProperty]
+		private string? noCarsMessage;
+
+		public IAsyncRelayCommand NavigateToCreateCarPageCommand { get; }
+		public IAsyncRelayCommand<Car> DeleteCarCommand { get; }
+
+
+		public MainPageViewModel(CarService carService)
+		{
+			_carService = carService;
+			NavigateToCreateCarPageCommand = new AsyncRelayCommand(NavigateToCreateCarPage);
+			DeleteCarCommand = new AsyncRelayCommand<Car>(DeleteCarAsync);
+			LoadCarsAsync();
+		}
+
+		private async Task DeleteCarAsync(Car car)
+		{
+			if (car == null)
+				return;
+
+			Cars?.Remove(car);
+
+			await _carService.DeleteCarByIdAsync(car.CarId);
+
+			await LoadItems();
+		}
+
+		public async Task LoadItems()
+		{
+			await LoadCarsAsync();
+		}
+
+		private async Task LoadCarsAsync()
+		{
+			var carsFromService = await _carService.GetAllCarsAsync();
+			Cars = new ObservableCollection<Car>(carsFromService);
+
+			NoCarsMessage = (Cars == null || Cars.Count == 0)
+				? "No cars yet!\n Add a new one to get started."
+				: null;
+		}
+
+		private async Task NavigateToCreateCarPage()
+		{
+			await Shell.Current.GoToAsync("//CreateCarPage");
+		}
+	}
+}
--- Road Pal/Views/MainPage.xaml.cs
+++ Road Pal/Views/MainPage.xaml.cs
@@ -1,13 +1,22 @@
-﻿namespace RoadPal.Views
+﻿using RoadPal.Services;
+using RoadPal.ViewModels;
+
+namespace RoadPal.Views
 {
 	public partial class MainPage : ContentPage
 	{
+		private readonly MainPageViewModel _viewModel;
 
-		public MainPage()
+		public MainPage(CarService carService)
 		{
 			InitializeComponent();
+			_viewModel = new MainPageViewModel(carService);
+			BindingContext = _viewModel;
 		}
-
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+			await _viewModel.LoadItems();
+		}
 	}
-
 }
