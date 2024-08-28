02b90f7c93488d5ec09596ce8727a2617eb34aad
venci0003 <venci0003@gmail.com>
2024-08-28 16:47:37 +0300

    Fixed MainPage.

--- Road Pal/Services/CarService.cs
+++ Road Pal/Services/CarService.cs
@@ -31,41 +31,5 @@ namespace RoadPal.Services
 		{
 			await _roadPalDatabase.GetConnection().Table<Car>().Where(c => c.CarId == id).DeleteAsync();
 		}
-
-
-		//public Task<List<Car>> GetAllCarsAsync()
-		//{
-		//	return _database.Table<Car>().ToListAsync();
-		//}
-
-		//public Task<int> AddCarAsync(Car car)
-		//{
-		//	return _database.InsertAsync(car);
-		//}
-
-		//public Task<Car> GetCarByIdAsync(int id)
-		//{
-		//	return _database.Table<Car>().Where(c => c.CarId == id).FirstOrDefaultAsync();
-		//}
-
-		//public Task<Car> GetCarByMakeAsync(string make)
-		//{
-		//	return _database.Table<Car>().Where(c => c.Make == make).FirstOrDefaultAsync();
-		//}
-
-		//public Task<int> UpdateCarAsync(Car car)
-		//{
-		//	return _database.UpdateAsync(car);
-		//}
-
-		//public Task<int> DeleteCarAsync(Car car)
-		//{
-		//	return _database.DeleteAsync(car);
-		//}
-
-		//public Task DeleteAllCarsAsync()
-		//{
-		//	return _database.ExecuteAsync("DELETE FROM Car");
-		//}
 	}
 }
