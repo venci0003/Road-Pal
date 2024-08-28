d118e75dbf62cea6c686cb609fe13eca4aa97d7d
venci0003 <venci0003@gmail.com>
2024-08-28 16:36:13 +0300

    Updated database methods and created Car service.

---
+++ Road Pal/Services/CarService.cs
@@ -0,0 +1,71 @@
+﻿using RoadPal.Infrastructure;
+using RoadPal.Infrastructure.Models;
+
+namespace RoadPal.Services
+{
+	public class CarService
+	{
+		private readonly RoadPalDatabase _roadPalDatabase;
+
+		public CarService(RoadPalDatabase context)
+		{
+			_roadPalDatabase = context;
+		}
+
+		public async Task AddCarAsync(Car car)
+		{
+			await _roadPalDatabase.GetConnection().InsertAsync(car);
+		}
+
+		public async Task<IEnumerable<Car>> GetAllCarsAsync()
+		{
+			return await _roadPalDatabase.GetConnection().Table<Car>().ToListAsync();
+		}
+
+		public async Task DeleteAllCarsTestAsync()
+		{
+			await _roadPalDatabase.DeleteAllCarsAsync();
+		}
+
+		public async Task DeleteCarByIdAsync(int id)
+		{
+			await _roadPalDatabase.GetConnection().Table<Car>().Where(c => c.CarId == id).DeleteAsync();
+		}
+
+
+		//public Task<List<Car>> GetAllCarsAsync()
+		//{
+		//	return _database.Table<Car>().ToListAsync();
+		//}
+
+		//public Task<int> AddCarAsync(Car car)
+		//{
+		//	return _database.InsertAsync(car);
+		//}
+
+		//public Task<Car> GetCarByIdAsync(int id)
+		//{
+		//	return _database.Table<Car>().Where(c => c.CarId == id).FirstOrDefaultAsync();
+		//}
+
+		//public Task<Car> GetCarByMakeAsync(string make)
+		//{
+		//	return _database.Table<Car>().Where(c => c.Make == make).FirstOrDefaultAsync();
+		//}
+
+		//public Task<int> UpdateCarAsync(Car car)
+		//{
+		//	return _database.UpdateAsync(car);
+		//}
+
+		//public Task<int> DeleteCarAsync(Car car)
+		//{
+		//	return _database.DeleteAsync(car);
+		//}
+
+		//public Task DeleteAllCarsAsync()
+		//{
+		//	return _database.ExecuteAsync("DELETE FROM Car");
+		//}
+	}
+}
--- RoadPal.Infrastructure/RoadPalDatabase.cs
+++ RoadPal.Infrastructure/RoadPalDatabase.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RoadPal.Infrastructure.Models;
+using SQLite;
+using static RoadPal.Common.ApplicationConstants;
 
 namespace RoadPal.Infrastructure
 {
-	internal class RoadPalDatabase
+	public class RoadPalDatabase
 	{
+		private readonly SQLiteAsyncConnection _database;
+
+		public RoadPalDatabase()
+		{
+			_database = new SQLiteAsyncConnection(DatabaseConstants.DatabasePath, DatabaseConstants.Flags);
+		}
+
+		public async Task InitializeAsync()
+		{
+			try
+			{
+				if (!_database.TableMappings.Any(m => m.MappedType.Name == typeof(Car).Name))
+				{
+					await _database.CreateTableAsync<Car>();
+				}
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.WriteLine($"Database initialization error: {ex.Message}");
+				throw;
+			}
+		}
+
+		public Task<int> DeleteAllCarsAsync()
+		{
+			return _database.ExecuteAsync("DELETE FROM Car");
+		}
+
+		public SQLiteAsyncConnection GetConnection()
+		{
+			InitializeAsync();
+			return _database;
+		}
 	}
 }
