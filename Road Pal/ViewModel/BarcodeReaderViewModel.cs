c9ed28dc07c847bfefc3c258d66708a92d083f58
venci0003 <venci0003@gmail.com>
2024-09-09 20:14:32 +0300

    Formatted receipt information from QR codes.

--- Road Pal/ViewModel/BarcodeReaderViewModel.cs
+++ Road Pal/ViewModel/BarcodeReaderViewModel.cs
@@ -34,9 +34,28 @@ namespace RoadPal.ViewModels
 			if (string.IsNullOrEmpty(barcodeInformation))
 				return;
 
+			string[] splittedInformation = barcodeInformation.Split('*');
+
+			string serialNumber = splittedInformation[0];
+
+			string vatId = splittedInformation[1];
+
+			string dateOfIssue = splittedInformation[2];
+
+			string timeOfIssue = splittedInformation[3];
+
+			string totalAmount = splittedInformation[4];
+
+			string receiptInfo = $"Receipt Information:\n" +
+								 $"Serial Number: {serialNumber}\n" +
+								 $"VAT ID: {vatId}\n" +
+								 $"Date of Issue: {dateOfIssue:yyyy-MM-dd}\n" +
+								 $"Time of Issue: {timeOfIssue:HH:mm:ss}\n" +
+								 $"Total Amount: {totalAmount} lv";
+
 			await Application.Current.MainPage.Dispatcher.DispatchAsync(async () =>
 			{
-				await Application.Current.MainPage.DisplayAlert("Barcode scanned successfully!", $"{barcodeInformation}", "Ok");
+				await Application.Current.MainPage.DisplayAlert("Barcode scanned successfully!", $"{receiptInfo}", "Ok");
 			});
 		}
 	}
