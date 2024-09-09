5db6f8bb72e592508cc8cb20557079cc52be5cfc
venci0003 <venci0003@gmail.com>
2024-09-09 20:08:50 +0300

    Added Barcode model.

---
+++ RoadPal.Infrastructure/Models/Barcode.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadPal.Infrastructure.Models
+{
+	public class Barcode
+	{
+		[PrimaryKey]
+		[AutoIncrement]
+		public int BarcodeId { get; set; }
+
+		[Required]
+		public int SerialNumber { get; set; }
+
+		public int VATId { get; set; }
+
+		public DateTime DateOfIssue { get; set; }
+
+		public DateTime TimeOfIssue { get; set; }
+
+        public decimal TotalAmount { get; set; }
+    }
+}
