77d4d9558d33488537ba8c70d956d78b7f4b0109
venci0003 <venci0003@gmail.com>
2024-09-11 22:59:13 +0300

    Added delay between barcode scans to prevent multiple scans of the same item.

--- Road Pal/Views/BarcodeReader.xaml.cs
+++ Road Pal/Views/BarcodeReader.xaml.cs
@@ -7,6 +7,9 @@ public partial class BarcodeReader : ContentPage
 {
 	private readonly BarcodeReaderViewModel _viewModel;
 
+	private bool _isScanningAllowed = true;
+	private const int ScanDelayMilliseconds = 3000;
+
 	public BarcodeReader(BarcodeReaderViewModel viewModel)
 	{
 		InitializeComponent();
@@ -17,19 +20,27 @@ public partial class BarcodeReader : ContentPage
 		{
 			Formats = BarcodeFormat.QrCode | BarcodeFormat.Code128 | BarcodeFormat.Code39 | BarcodeFormat.Ean13,
 			AutoRotate = true,
-			TryHarder = true
+			TryHarder = true,
 		};
 	}
 
 	private async void BarcodeReader_BarcodesDetected(object sender, BarcodeDetectionEventArgs e)
 	{
-		var first = e.Results?.FirstOrDefault();
+		if (_isScanningAllowed && e.Results.Any())
+		{
+			_isScanningAllowed = false;
+			var first = e.Results?.FirstOrDefault();
 
-		if (first is null)
-			return;
+			if (first is null)
+				return;
 
-		var viewModel = (BarcodeReaderViewModel)BindingContext;
-		await viewModel.BarcodeScannedCommand.ExecuteAsync(first.Value);
+			var viewModel = (BarcodeReaderViewModel)BindingContext;
+			await viewModel.BarcodeScannedCommand.ExecuteAsync(first.Value);
+
+			await Task.Delay(ScanDelayMilliseconds);
+
+			_isScanningAllowed = true;
+		}
 	}
 
 	private void ToggleTorchButton_Clicked(object sender, EventArgs e)
