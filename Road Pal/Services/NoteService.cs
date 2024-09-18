49da82d18e00b182a68752af8f58a1a999ab1dda
venci0003 <venci0003@gmail.com>
2024-09-18 21:25:43 +0300

    Added  NoteService.

---
+++ Road Pal/Services/NoteService.cs
@@ -0,0 +1,23 @@
+﻿using RoadPal.Infrastructure;
+using RoadPal.Infrastructure.Models;
+using SQLite;
+
+namespace RoadPal.Services
+{
+	public class NoteService
+	{
+		private readonly RoadPalDatabase _roadPalDatabase;
+
+		public NoteService(RoadPalDatabase context)
+		{
+			_roadPalDatabase = context;
+		}
+
+		public async Task AddServiceNote(ServiceNote serviceNote)
+		{
+			SQLiteAsyncConnection connection = await _roadPalDatabase.GetConnectionAsync();
+			await connection.InsertAsync(serviceNote);
+		}
+
+	}
+}
