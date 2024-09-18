626e2232e5b532e262738baa85516f05007ff87c
venci0003 <venci0003@gmail.com>
2024-09-18 21:31:00 +0300

    Updated ServiceNote model to have manual foreign key relation with Car model.

--- RoadPal.Infrastructure/Models/ServiceNote.cs
+++ RoadPal.Infrastructure/Models/ServiceNote.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
 
 namespace RoadPal.Infrastructure.Models
 {
 	public class ServiceNote
 	{
+		[AutoIncrement]
+		[PrimaryKey]
 		public int ServiceNoteId { get; set; }
 		public string Title { get; set; }
 		public string Description { get; set; }
 		public DateTime CreatedDate { get; set; }
+
+		public int CarId { get; set; }
 	}
 }
