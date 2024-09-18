428e57feee3f567eae3a9b0fb231ead43a5a4a51
venci0003 <venci0003@gmail.com>
2024-09-18 15:10:12 +0300

    Added service note model.

---
+++ RoadPal.Infrastructure/Models/ServiceNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadPal.Infrastructure.Models
+{
+	public class ServiceNote
+	{
+		public int ServiceNoteId { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime CreatedDate { get; set; }
+	}
+}
