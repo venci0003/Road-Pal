0056e81f1aeb3fb218cb4e8fa2eb6b71b3c2d742
venci0003 <venci0003@gmail.com>
2024-09-25 16:15:52 +0300

    Added on tap changes to colors of both buttons in service notes section.

--- Road Pal/Views/CarDetailsPage.xaml.cs
+++ Road Pal/Views/CarDetailsPage.xaml.cs
@@ -18,4 +18,24 @@ public partial class CarDetailsPage : ContentPage
 		base.OnAppearing();
 		await _viewModel.LoadDetailsAsync();
 	}
+
+	private void OnUnfinishedTapped(object sender, EventArgs e)
+	{
+		UnfinishedFrame.BackgroundColor = Color.FromArgb("#171E25");
+		FinishedFrame.BackgroundColor = Color.FromArgb("#3A4755");
+
+		// Optionally change text color
+		//((Label)UnfinishedFrame.Content.FindByName("UnfinishedLabel")).TextColor = Colors.White;
+		//((Label)FinishedFrame.Content.FindByName("FinishedLabel")).TextColor = Colors.Black;
+	}
+
+	private void OnFinishedTapped(object sender, EventArgs e)
+	{
+		UnfinishedFrame.BackgroundColor = Color.FromArgb("#3A4755");
+		FinishedFrame.BackgroundColor = Color.FromArgb("#171E25");
+
+		// Optionally change text color
+		//((Label)UnfinishedFrame.Content.FindByName("UnfinishedLabel")).TextColor = Colors.Black;
+		//((Label)FinishedFrame.Content.FindByName("FinishedLabel")).TextColor = Colors.White;
+	}
 }
